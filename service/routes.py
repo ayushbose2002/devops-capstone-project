41da6fbcb41064099bb0d2f2151ce0581195b335
ayushbose2002 <ayushbose.1013@gmail.com>
2024-11-07 14:37:56 -0500

    Add test_invalid_account_id_format

--- service/routes.py
+++ service/routes.py
@@ -101,7 +101,7 @@ def get_accounts(account_id):
     except ValueError:
         # Return 400 Bad Request if account_id is not an integer
         return jsonify({"error": "Invalid account ID format, must be an integer"}), status.HTTP_400_BAD_REQUEST
-    
+
     app.logger.info("Request to read an Account with id: %s", account_id)
     account = Account.find(account_id)
     if not account:
